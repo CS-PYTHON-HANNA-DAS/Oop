a1b4a1f81cea0f52b2efe5cf755e05be2e9f7708
CS-PYTHON-HANNA-DAS <cosannadashkevich@gmail.com>
2026-09-09 12:54:15 +0200

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -4,10 +4,13 @@ class Pracownik:
         self.pensja = pensja
         
     def podwyzka(self, procent):
-        self.pensja += self.pensja * (procent / 100)
+        self.pensja += (self.pensja) * (procent / 100)
 
 # Проверка (эффект конечный):
 p1 = Pracownik("Ola", 5000)
 p1.podwyzka(10)
 print(p1.pensja)
-# Выведет: 5500.0
+
+#1. Конструктор (⁠__init__⁠): принимает имя и зарплату, сохраняя их в поля ⁠self.imie⁠ и ⁠self.pensja⁠.
+#2. Метод ⁠podwyzka⁠: рассчитывает прибавку как процент от текущей зарплаты и прибавляет её к ⁠self.pensja⁠.
+
